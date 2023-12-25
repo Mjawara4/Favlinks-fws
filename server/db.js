a11b40f7496dd21922a267fbc90c4c30fc203e4c
Mjawara4 <101847266+Mjawara4@users.noreply.github.com>
2023-12-24 22:50:28 -0500

    update for hw6

---
+++ server/db.js
@@ -0,0 +1,65 @@
+
+
+const pool = require('pg').pool
+const pool = new pool({
+    user: 'mahaa',
+    host: 'localhost',
+    database: 'favlinks',
+    password: 2552,
+    port: 5432,
+})
+
+// Create a link
+const createLink = (req,res)=>{
+    const name = req.body.name;
+    const url = req.body.url;
+
+    pool.query('INSERT INTO links(name, url) VALUES($1, $2)',[name, url], (error, result)=>{
+        if(error){
+            throw error;
+        }
+        console.log(req.body);
+        res.send("Link successfully added :)")
+    });
+}
+//Read From table links
+const getLinks = (req,res)=>{
+    pool.query('SELECT * FROM links ORDER BY id ASC', (error, result)=>{
+        if(error){
+            throw error;
+        }
+        res.status(200).json(result.rows)
+    });
+}
+//UPDATE LINKS
+const updateLink = (req,res)=>{
+    const id = parseInt(req.params.id);
+    // const name = req.body.name;
+    // const url = req.body.url;
+    const {name, url} = req.body;
+
+    pool.query('UPDATE links SET name = $1, url = $2 WHERE id = $3',[name, url, id], (error, results)=>{
+        if(error){
+            throw error;
+        }
+        res.status(201).send(`Link with ID: ${id} was succesfully updated.`)
+    });
+}
+//DELETE A LINK
+const deleteLink = (req,res)=>{
+    const id = parseInt(req.params.id)
+    pool.query('DELETE FROM links WHERE id = $1',[id], (error, results)=>{
+        if(error){
+            throw error;
+        }
+        res.status(201).send(`Link ${id} successfully removed.`)
+    });
+}
+//Export functions
+
+module.exports ={
+    getLinks,
+    createLink,
+    updateLink,
+    deleteLink
+}
--- server/index.js
+++ server/index.js
@@ -2,15 +2,27 @@ const { DiffieHellmanGroup } = require('crypto')
 const express = require('express')
 const path = require('path')
 const app = express()
+const cors = require('cors')
 const PORT = 3000
 const clientPath = path.resolve(__dirname, '../client/dist')
 const db = require('/.db')
-app.use(express.static(clientPath))
 
+
+app.use(express.static(clientPath))
+app.use(cors())
+
+app.get('/api/links', db.getLinks);
 app.get('/', (req, res) => {
     // we'll do some stuff here
     res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'))
 })
+
+
+app.get('/api/links', db.getLinks);
+app.post('/api/links', db.createLink);
+app.put('/api/links/:id', db.updateLink);
+app.delete('/api/links/:id', db.deleteLink);
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
